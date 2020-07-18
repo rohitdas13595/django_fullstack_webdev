bb45cb9df7c3923db100f896a3d3b55d828d5293
rohitdas13595 <rohitdas10595@gmail.com>
2020-07-18 11:33:09 +0530

    teplates for groups app

--- SocialMediaSite/simplesocial/groups/admin.py
+++ SocialMediaSite/simplesocial/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+class GroupMemberInline(admin.TabularInline):
+    model= models.GroupMember
+    
+admin.site.register(model.Group)
